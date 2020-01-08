c3ce1230b0ddfa7416b8c14d56cca398fc08d8f8
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2020-01-08 14:55:11 -0500

    Update sandbox

--- code/test.py
+++ code/test.py
@@ -3,14 +3,17 @@
 import stanfordnlp
 from models import Event
 
+content = "First, President Bush occupied Iraq. Then, Iraq was offered a deal by him" 
+
 nlp = stanfordnlp.Pipeline(processors='tokenize,mwt,pos,lemma,depparse', lang='en')
-doc = nlp("First, President Bush occupied Iraq. Then, he offered Iraq a deal.")
+doc = nlp(content)
 events = set()
 
 for sentence in doc.sentences:
     for word in sentence.words:
         print(word.pos, word.text, "->", word. dependency_relation, sentence.words[word.governor - 1].text if word.governor > 0 else 'root')
-        if word.pos == "VBD":
+        
+        if word.pos == "VBD"  
             verb, argument = word.text, sentence.words[word.governor - 1].text if word.governor > 0 else 'root'
             event = Event(verb, argument)
             events.add(event)
