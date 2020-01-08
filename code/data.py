2f1126e22042afa0df333222c9d3ef578821e7d0
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2020-01-08 15:33:36 -0500

    Add agiga data cleaning step

--- code/data.py
+++ code/data.py
@@ -4,7 +4,11 @@ class DataLoader:
     def __init__(self, path):
         self.path = path
         with open(path) as f:
-            self.text = f.read()        
+            self.text = f.read()  
+
+    """removes execution metatdata from plaintext"""
+    def clean_agiga_text(self):
+        self.text = " ".join(self.get_lines()[13:-7])
 
     def get_text(self):
         return self.text
@@ -12,3 +16,8 @@ class DataLoader:
     def get_lines(self):
         lines = self.text.split("\n")
         return lines
+
+def build_loader():
+    loader = DataLoader("data/agiga/export.txt")
+    loader.clean_agiga_text()
+    return loader
